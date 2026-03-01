560d4f326dd47460adba2d9a82d64ef0ce5085e0
yonatanfarhi20 <yonatanfarhi1@gmail.com>
2026-03-01 14:37:29 +0200

    Step 2: Added broadcast and remove functions with error handling

--- server.py
+++ server.py
@@ -11,3 +11,21 @@ clients = []
 nicknames = []
 
 print(f"Server is listening on {HOST}:{PORT}...")
+
+def remove(client):
+        index = clients.index(client)
+        clients.remove(client)
+        client.close()
+        nickname = nicknames[index]
+        nicknames.remove(nickname)
+        broadcast(f'{nickname} left the chat!'.encode('utf-8'), None)
+
+def broadcast(message, sender_client):
+    for client in clients:
+        if client != sender_client:
+            try:
+                client.send(message)
+            except Exception as e:
+                print(f"Connection error: {e}")
+                remove(client)
+
